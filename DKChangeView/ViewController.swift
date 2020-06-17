bc208a9c749e0d7e1e9563543e7e9bbf840a2e6a
David Kuo <h124079274@msn.com>
2020-06-17 18:01:49 +0800

    no message

--- DKChangeView/ViewController.swift
+++ DKChangeView/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        title = "NavigationBar"
     }
 
     @IBAction func performSegue(_ sender: Any)
